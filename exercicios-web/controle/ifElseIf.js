4ba671ac380c69d3e971b8fa5852d080425a9976
Carlos Eduardo Fraga Ribeiro <eduardo@eftech.com.br>
2019-05-01 17:09:50 -0300

    Update ifElseIf.js

--- exercicios-web/controle/ifElseIf.js
+++ exercicios-web/controle/ifElseIf.js
@@ -16,9 +16,9 @@ const imprimirResultado = function (nota) {
     }
 }
 
-imprimirResultado(10)
-imprimirResultado(8.9)
-imprimirResultado(6.55)
-imprimirResultado(2.3)
-imprimirResultado(-1)
-imprimirResultado(11)
+
+
+for (i = 10; i >= -1; i--) {
+    imprimirResultado(i)
+}
+
